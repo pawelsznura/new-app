014c322a96a9c416a4ed6c56bf0dc69273885e6b
Pawel <p.sznura@rgu.ac.uk>
2023-02-16 14:11:57 +0000

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: ''};
+    this.valChange = this.valChange.bind(this);
+    this.valSubmit = this.valSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+      <h1>My Fancy To Do List</h1>
+      <TodoList items={this.state.items} />
+      <form onSubmit={this.valSubmit}>
+       <label htmlFor="new-todo">
+        What should we do next....?
+       </label>
+       <input
+         id="new-todo"
+         onChange={this.valChange}
+         value={this.state.text}
+        />
+        <button>
+          Add #{this.state.items.length +1}
+        </button>
+      </form>
+      </div>
+    );
+  }
+
+  valChange(e) {
+    this.setState({text: e.target.value})
+  }
+
+  valSubmit(e) {
+    e.preventDefault();
+    if(!this.state.text.length){
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
 }
 
-export default App;
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item, index) => (
+          <li key={index}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default MyToDoList;
